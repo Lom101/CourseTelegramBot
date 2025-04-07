36179054a2881ffd26ac4812c9331810c47aa770
Bulat Shakirov <shakirov.bulat.contact@gmail.com>
2025-04-08 01:06:08 +0300

    bot code added

--- Bot/Program.cs
+++ Bot/Program.cs
@@ -1,1 +1,30 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Telegram.Bot;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using DotNetEnv;
+
+Env.Load();
+Env.TraversePath().Load();
+var botToken = Environment.GetEnvironmentVariable("TOKEN") ?? throw new ArgumentNullException("Не указан телеграм бот токен");
+
+var botClient = new TelegramBotClient(botToken);
+botClient.StartReceiving(
+    async (bot, update, token) =>
+    {
+        if (update.Message?.Text != null)
+        {
+            Console.WriteLine($"Получено сообщение от {update.Message.Chat.Id}: {update.Message.Text}");
+            await bot.SendTextMessageAsync(update.Message.Chat.Id, $"Ты написал: {update.Message.Text}", cancellationToken: token);
+        }
+    },
+    (bot, exception, token) =>
+    {
+        Console.WriteLine($"Ошибка: {exception.Message}");
+        return Task.CompletedTask;
+    },
+    new ReceiverOptions { AllowedUpdates = Array.Empty<UpdateType>() }
+);
+
+Console.WriteLine($"Бот запущен: @{(await botClient.GetMeAsync()).Username}");
+Console.ReadLine();
