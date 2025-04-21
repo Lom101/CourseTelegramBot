bd9a951749dbc805de826877d8cf8e783f27c17c
Bulat Shakirov <shakirov.bulat.contact@gmail.com>
2025-04-21 20:42:00 +0300

    test entity added

---
+++ Core/Entity/TestQuestion.cs
@@ -0,0 +1,9 @@
+﻿namespace Core.Entity;
+
+public class TestQuestion
+{
+    public int Id { get; set; }
+    public string QuestionText { get; set; }
+    public List<string> Options { get; set; } // Варианты ответа
+    public int CorrectIndex { get; set; } // Индекс правильного варианта
+}
