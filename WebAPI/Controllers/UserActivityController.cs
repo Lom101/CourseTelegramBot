4bedcd27a711cbb5174d7e29fe221ec042112462
Bulat Shakirov <shakirov.bulat.contact@gmail.com>
2025-04-24 00:57:12 +0300

    docs

--- WebAPI/Controllers/UserActivityController.cs
+++ WebAPI/Controllers/UserActivityController.cs
@@ -4,11 +4,16 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Backend.Controllers;
 
-
+/// <summary>
+/// Контроллер для получения статистики активности пользователей
+/// </summary>
 [ApiController]
 [Route("api/[controller]")]
 public class UserActivityController(IUserActivityRepository activityRepository) : ControllerBase
 {
+    /// <summary>
+    /// Получить активность пользователя
+    /// </summary>
     [HttpGet("{id}")]
     public async Task<IActionResult> GetUserActivityByUserId(int userId)
     {
--- WebAPI/Controllers/UserProgressController.cs
+++ WebAPI/Controllers/UserProgressController.cs
@@ -4,10 +4,16 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Backend.Controllers;
 
+/// <summary>
+/// Контроллер для статистикой прогресса пользователя
+/// </summary>
 [ApiController]
 [Route("api/[controller]")]
 public class UserProgressController(IUserProgressRepository progressRepository) : ControllerBase
 {
+    /// <summary>
+    /// Получить прогресс пользователя по курсу
+    /// </summary>
     [HttpGet("{userId:long}")]
     public async Task<IActionResult> GetUserProgressByUserId(int userId)
     {
