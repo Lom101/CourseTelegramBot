8623fc1cb596a3f5a87ae7c482bbf76a3f61f67e
sabi <sabina.mini04@gmail.com>
2025-04-23 11:59:08 +0300

    need

--- WebAPI/Controllers/TopicController.cs
+++ WebAPI/Controllers/TopicController.cs
@@ -6,29 +6,29 @@ namespace Backend.Controllers;
 
 [ApiController]
 [Route("api/[controller]")]
-public class TopicController(ITopicRepository topicRepository) : ControllerBase
+public class TopicsController(ITopicRepository topicRepository) : ControllerBase
 {
-    [HttpGet("by-course/{id}")]
-    public async Task<IActionResult> GetTopicByCourseId(int courseId)
+    
+    
+    [HttpGet("by-course/{courseId:int}")]
+    public async Task<ActionResult<IEnumerable<Topic>>> GetByCourseId(int courseId)
     {
         var topics = await topicRepository.GetByCourseIdAsync(courseId);
         return topics.Any() ? Ok(topics) : throw new KeyNotFoundException($"No topics found for course {courseId}");
     }
 
-    [HttpGet("{id}")]
-    public async Task<IActionResult> GetTopicById(int id)
+    [HttpGet("{id:int}")]
+    public async Task<ActionResult<Topic>> GetById(int id)
     {
         var topic = await topicRepository.GetByIdAsync(id);
-        if (topic == null)
-            throw new KeyNotFoundException($"Topic with id {id} not found");
-
-        return Ok(topic);
+        return topic ?? throw new KeyNotFoundException($"Topic with id {id} not found");
     }
     
     [HttpPost]
     public async Task<IActionResult> CreateTopic([FromBody] Topic topic)
     {
         await topicRepository.AddAsync(topic);
-        return CreatedAtAction(nameof(GetTopicById), new { id = topic.Id }, topic);
+        return CreatedAtAction(nameof(GetById), new { id = topic.Id }, topic);
     }
+    
 }
